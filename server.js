c18e979abc4dfeb1e905253c6d3ddf8e0329a196
SarikaJhaa6 <sarikajhaa6@gmail.com>
2025-03-07 23:07:33 +0530

    cors-updated

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins (Not recommended for production)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
